efc03963ebaab420829f553c22b3767d98068a57
Lestat Clemmer <97067840+lestatclemmer@users.noreply.github.com>
2024-10-15 19:51:43 -0500

    Update 7-2.sh
    same

--- 7-2.sh
+++ 7-2.sh
@@ -1,4 +1,4 @@
-chown --from lfs -R root:root $LFS/{usr,lib,var,etc,bin,sbin,tools,sources}
+chown --from lfs -R root:root $LFS/{usr,lib,var,etc,bin,sbin,tools}
 case $(uname -m) in
   x86_64) chown --from lfs -R root:root $LFS/lib64 ;;
 esac
