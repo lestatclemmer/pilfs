f7443c7753d3dd7720273f4c8d9235b631756b62
Lestat Clemmer <97067840+lestatclemmer@users.noreply.github.com>
2024-10-15 19:57:13 -0500

    Add files via upload

---
+++ 2-4.sh
@@ -0,0 +1,7 @@
+echo "export $LFS=/mnt/lfs" >> .profile
+$LFS=/mnt/lfs
+mkdir -v $LFS/sources
+chmod -v a+wt $LFS/sources
+cd $LFS/sources
+wget https://intestinate.com/pilfs/scripts/wget-list-sysv
+wget --input-file=wget-list-sysv --continue --directory-prefix=$LFS/sources --no-check-certificate
