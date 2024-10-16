e884a5f2380ee53cab6672c866620d9fe55d19ea
Lestat Clemmer <97067840+lestatclemmer@users.noreply.github.com>
2024-10-15 19:58:14 -0500

    Update 2-4.sh

--- 2-4.sh
+++ 2-4.sh
@@ -3,5 +3,5 @@ $LFS=/mnt/lfs
 mkdir -v $LFS/sources
 chmod -v a+wt $LFS/sources
 cd $LFS/sources
-wget https://intestinate.com/pilfs/scripts/wget-list-sysv
+wget https://intestinate.com/pilfs/scripts/wget-list-sysv --no-check-certificate
 wget --input-file=wget-list-sysv --continue --directory-prefix=$LFS/sources --no-check-certificate
